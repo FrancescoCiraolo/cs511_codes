c049b033b59225228d5952277ce58de3af304c90
Francesco Ciraolo <fciraolo@bu.edu>
2023-09-27 23:54:41 -0400

    submission HW3

--- ciraolo.francesco.hw03e5.lean
+++ ciraolo.francesco.hw03e5.lean
@@ -7,38 +7,53 @@ import Library.Tactic.Addarith
 import Library.Tactic.Cancel
 import Library.Tactic.Use
 
-example (x : ℝ) (hx : x > 0) : x^2 > x :=
-  have hx_squared_pos : x^2 > 0 := pow_pos hx 2
-  apply lt_trans hx hx_squared_pos
+-- example (x : ℝ) (hx : x > 0) : x^2 > x := by
+--   have hx_squared_pos : x^2 > 0 := pow_pos hx 2
+--   apply lt_trans hx hx_squared_pos
 
-example (x : ℚ) : ∃ y : ℚ, y ^ 2 > x := by
-  have H := le_or_gt x 0
-  obtain hx | hx := H
-  · use 1
-    linarith
-  · use (x + 1)
-    simp
-    have px: 0 ≤ x := by exact LT.lt.le hx
-    have sqx := calc
-        x = x^1 := by simp
-        _ < x^2 := pow_lt_pow_of_lt_left (lt_add_one x) px,
-    have hx' := sq_pos_of_pos hx 
-    have x' := sq_nonneg x
-    have x'' := le_of_eq (sq_nonneg x)
-    have hx'' := lt_of_le_of_lt (le_of_eq (sq_nonneg x)) (sq_pos_of_pos hx)
-    sorry
-    -- apply sq_pos_of_pos
-    -- exact lt_of_le_of_lt (le_of_eq (sq_nonneg x)) (sq_pos_of_pos hx)
+-- example (x : ℚ) : ∃ y : ℚ, y ^ 2 > x := by
+--   have H := le_or_gt x 0
+--   obtain hx | hx := H
+--   · use 1
+--     linarith
+--   · use (x + 1)
+--     simp
+--     have px: 0 ≤ x := by exact LT.lt.le hx
+--     have sqx := calc
+--         x = x^1 := by simp
+--         _ < x^2 := pow_lt_pow_of_lt_left (lt_add_one x) px,
+--     have hx' := sq_pos_of_pos hx 
+--     have x' := sq_nonneg x
+--     have x'' := le_of_eq (sq_nonneg x)
+--     have hx'' := lt_of_le_of_lt (le_of_eq (sq_nonneg x)) (sq_pos_of_pos hx)
+--     sorry
+--     -- apply sq_pos_of_pos
+--     -- exact lt_of_le_of_lt (le_of_eq (sq_nonneg x)) (sq_pos_of_pos hx)
     
 
-example {t : ℝ} (h : ∃ a : ℝ, a * t + 1 < a + t) : t ≠ 1 := by
-  obtain ⟨x, hxt⟩ := h
-  have H := le_or_gt (x * t) (x + t)
-  obtain hx | hx := H
-  · 
-    sorry
-  · 
-    sorry
+-- example {t : ℝ} (h : ∃ a : ℝ, a * t + 1 < a + t) : t ≠ 1 := by
+--   obtain ⟨x, hxt⟩ := h
+--   have H := le_or_gt x 0
+--   obtain hx | hx := H
+--   · have H := lt_or_eq_of_le hx
+--     obtain hx | hx := H
+--     · 
+--       sorry
+--     · apply ne_of_gt
+--       have hxt' : calc
+--         1 = 1 + (0 * t) := by ring
+--         _ = 1 + (x * t) := by rel[hx] 
+--   · sorry
+--   -- have H := le_or_gt (x * t) 0
+--   -- obtain hx | hx := H
+--   -- · have H := lt_or_eq_of_le hx
+--   --   obtain hx | hx := H
+--   --   · 
+--   --     sorry
+--   --   ·  
+--   --     sorry
+--   -- · 
+--   --   sorry
 
 example {m : ℤ} (h : ∃ a, 2 * a = m) : m ≠ 5 := by
   obtain ⟨x, hxt⟩ := h

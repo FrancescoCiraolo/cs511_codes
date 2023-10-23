4ae13ae20bf6a9351e11bd5ce0f41faf575f9e81
Francesco Ciraolo <fciraolo@bu.edu>
2023-10-23 10:46:32 -0400

    HW07

---
+++ ciraolo.francesco.hw07e4.lean
@@ -0,0 +1,41 @@
+import Mathlib.Data.Real.Basic
+import Mathlib.Tactic.IntervalCases
+import Library.Theory.Comparison
+import Library.Theory.Parity
+import Library.Theory.Prime
+import Library.Tactic.Rel
+import Library.Tactic.ModCases
+import Library.Tactic.Extra
+import Library.Tactic.Numbers
+import Library.Tactic.Addarith
+import Library.Tactic.Cancel
+import Library.Tactic.Use
+
+
+-- 4 (a)
+example : ¬ (∃ n : ℕ, n ^ 2 = 2) := by
+  push_neg
+  intro n
+  have hn := le_or_succ_le n 1
+  obtain hn | hn := hn
+  · apply ne_of_lt
+    calc
+      n ^ 2 ≤ 1 ^ 2 := by rel [hn]
+      _ < 2 := by numbers
+  · apply ne_of_gt
+    calc
+      2 ≤ n :=  hn
+      _ < n + n := by extra
+      _ = 2 * n := by ring
+      _ ≤ n * n := by rel[hn]
+      _ = n^2 := by ring
+
+-- 4 (b)
+
+example (P Q : Prop) : ¬ (P → Q) ↔ (P ∧ ¬ Q) := by
+  push_neg
+  constructor
+  · intros h
+    apply h
+  · intros h
+    apply h
---
+++ ciraolo.francesco.hw07e5.lean
@@ -0,0 +1,35 @@
+import Mathlib.Data.Real.Basic
+import Mathlib.Tactic.IntervalCases
+import Library.Theory.Comparison
+import Library.Theory.Parity
+import Library.Theory.Prime
+import Library.Tactic.Rel
+import Library.Tactic.ModCases
+import Library.Tactic.Extra
+import Library.Tactic.Numbers
+import Library.Tactic.Addarith
+import Library.Tactic.Cancel
+import Library.Tactic.Use
+
+-- 5(a)
+example {p : ℕ} (k : ℕ) (hk1 : k ≠ 1) (hkp : k ≠ p) (hk : k ∣ p) : ¬ Prime p := by
+  dsimp [Prime]
+  push_neg
+  intros hp
+  use k
+  constructor
+  · apply hk  
+  constructor
+  · apply hk1
+  · apply hkp
+
+-- 5(b)
+example {p : ℕ} (hp : ¬ Prime p) (hp2 : 2 ≤ p) : ∃ m, 2 ≤ m ∧ m < p ∧ m ∣ p := by
+  
+  have H : ¬ (∀ (m : ℕ), 2 ≤ m → m < p → ¬m ∣ p)
+  · intro H
+    have hp' := prime_test hp2 H
+    contradiction
+    
+  push_neg at H
+  apply H
